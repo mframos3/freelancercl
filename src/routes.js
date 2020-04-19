1faf9dc991eaffdd944063f44e46d3308fd00065
NICOLAS CASASSUS <ncasassus@uc.cl>
2020-04-19 18:05:03 -0400

    :sparkles: tabla_intermedia_reviews

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const hello = require('./routes/hello');
 const index = require('./routes/index');
 const reports = require('./routes/reports');
 const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 const router = new KoaRouter();
 
@@ -11,5 +12,6 @@ router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/reports', reports.routes());
 router.use('/users', users.routes());
+router.use('/reviews', reviews.routes());
 
 module.exports = router;
