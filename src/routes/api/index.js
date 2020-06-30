3f59352eb832122e7f68bf347f370680dd511d6c
mframos3 <mframos3@uc.cl>
2020-06-30 18:39:19 -0400

    posts restapi

---
+++ src/routes/api/index.js
@@ -0,0 +1,24 @@
+const KoaRouter = require('koa-router');
+const jwt = require('koa-jwt');
+
+const authApi = require('./auth');
+const chatApi = require('./chat');
+const followApi = require('./follow');
+const postsApi = require('./posts');
+
+const router = new KoaRouter();
+
+router.use('/auth', authApi.routes());
+
+router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
+router.use(async (ctx, next) => {
+  if (ctx.state.authData.userId) {
+    ctx.state.currentUser = await ctx.orm.user.findByPk(ctx.state.authData.userId);
+  }
+  return next();
+});
+
+router.use('/chat', chatApi.routes());
+router.use('/follow', followApi.routes());
+router.use('/posts', postsApi.routes());
+module.exports = router;
