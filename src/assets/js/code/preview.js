cc99d632d24c2b1080e2faf70dfe2a1d5ee3c170
mframos3 <mframos3@uc.cl>
2020-07-03 00:25:35 -0400

    Profile improvements

--- src/assets/js/code/preview.js
+++ src/assets/js/code/preview.js
@@ -1,11 +1,17 @@
 $(document).ready(() => {
-  const pdfFile = document.getElementById('CV').files[0];
-  if (pdfFile) {
+  $('#vista').hide();
+
+  function readURL() {
+    const pdfFile = document.getElementById('CV').files[0];
     const pdfFileUrl = URL.createObjectURL(pdfFile);
     $('#viewer').attr('src', pdfFileUrl);
-    $('#viewerDefault').attr('height', '0');
-  } else {
-    $('#viewerDefault').attr('src', 'https://freelancercl.sfo2.digitaloceanspaces.com/noFile.png');
-    $('#viewer').attr('height', '0');
   }
+
+  $('#vista').click(() => {
+    readURL();
+  });
+
+  $('#CV').change(() => {
+    $('#vista').show();
+  });
 });
